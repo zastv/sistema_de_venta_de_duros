fd86f8f6a860639fe86015a8bd36e068fba0e5a5
zastv <132970150+zastv@users.noreply.github.com>
2026-04-03 23:38:36 +0000

    aguita con pan

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://incftkpsdlslfwhlpype.supabase.co';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluY2Z0a3BzZGxzbGZ3aGxweXBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyNjA4NDAsImV4cCI6MjA3MTgzNjg0MH0.9ON2x3isNJogWcVoA5HxNLXQIHIil_lHJeTCmemfxDg';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'sb_publishable_B-v269PyKwTZve3GCwrmrA_7C4U5Z9Y';
 
 // Configurar cliente con opciones adicionales para debugging
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
